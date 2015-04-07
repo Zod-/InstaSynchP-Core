1eabab1beefc635d7a59e5bad3176de063da3331
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-04-07 19:06:45 +0200

    Preparing for css builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,12 +13,31 @@ module.exports = function (grunt) {
             pattern: /@VERSION@/g,
             replacement: '<%= pkg.version %>'
           }, {
+            pattern: /@CORECSSREV@/g,
+            replacement: function () {
+              var cssrev = grunt.file.read('dist/coreCSSrev')
+                .trim();
+              grunt.file.delete('dist/coreCSSrev');
+              return cssrev;
+            }
+          }, {
+            pattern: /@RAWGITREPO@/g,
+            replacement: 'https://cdn.rawgit.com/Zod-/InstaSynchP-Core'
+          }, {
             pattern: /\/\/ @name[^\n]*\n/g,
             replacement: function (match) {
               return match.replace(/-/, ' ');
             }
           }]
         }
+      }
+    },
+    copy: {
+      dist: {
+        flatten: true,
+        expand: true,
+        src: ['src/core.css'],
+        dest: 'dist/',
       }
     },
     jshint: {
@@ -30,7 +49,7 @@ module.exports = function (grunt) {
     },
     concat: {
       dist: {
-        src: ['src/meta.js','src/events.js', 'src/core.js'],
+        src: ['src/meta.js', 'src/events.js', 'src/core.js'],
         dest: 'dist/InstaSynchP-Core.user.js'
       }
     },
@@ -38,19 +57,32 @@ module.exports = function (grunt) {
       build: {
         dest: 'src/meta.js'
       }
-    }
+    },
+    shell: {
+      gitlog: {
+        command: 'git log -n 1 --pretty="%H" dist/core.css',
+        options: {
+          callback: function log(err, stdout, stderr, cb) {
+            grunt.file.write('dist/coreCSSrev', stdout);
+            cb();
+          }
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-string-replace');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-userscript-meta');
+  grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['userscript-meta', 'concat',
-    'string-replace', 'jshint'
-  ]);
-  grunt.registerTask('test', ['concat', 'jshint']);
-  grunt.registerTask('build', ['userscript-meta', 'concat',
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('build-css', ['copy']);
+  grunt.registerTask('build-js', ['shell', 'userscript-meta', 'concat',
     'string-replace'
   ]);
+  grunt.registerTask('build', ['build-css', 'build-js']);
+  grunt.registerTask('default', ['build', 'test']);
 };
