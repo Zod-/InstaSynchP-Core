f2242c7f26581466a8ac715e5f75074edfdcd7bc
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-04-08 18:34:17 +0200

    Updated InstaSynchP-Library for the logs

--- src/meta.js
+++ src/meta.js
@@ -10,7 +10,7 @@
 // @require      https://greasyfork.org/scripts/2855-gm-config/code/code.js?version=33973
 // @require      https://greasyfork.org/scripts/2857-jquery-bind-first/code/code.js?version=26080
 // @require      https://greasyfork.org/scripts/8159-log4javascript/code/code.js?version=37575
-// @require      https://greasyfork.org/scripts/5647-instasynchp-library/code/code.js?version=37716
+// @require      https://greasyfork.org/scripts/5647-instasynchp-library/code/code.js?version=41059
 // @require      https://greasyfork.org/scripts/8177-instasynchp-logger/code/code.js?version=37872
 // @require      https://greasyfork.org/scripts/6573-instasynchp-plugin-manager/code/code.js?version=42665
 // @require      https://greasyfork.org/scripts/5718-instasynchp-cssloader/code/code.js?version=43457
