730b95a204b3f378b7fdf7f524700b2d9faf0a29
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-04-18 11:01:44 +0200

    New Event Hooks version

--- src/meta.js
+++ src/meta.js
@@ -16,7 +16,7 @@
 // @require      https://greasyfork.org/scripts/5718-instasynchp-cssloader/code/code.js?version=43457
 // @require      https://greasyfork.org/scripts/5719-instasynchp-settings/code/code.js?version=42666
 // @require      https://greasyfork.org/scripts/6332-instasynchp-commands/code/code.js?version=37738
-// @require      https://greasyfork.org/scripts/5651-instasynchp-event-hooks/code/code.js?version=38337
+// @require      https://greasyfork.org/scripts/5651-instasynchp-event-hooks/code/code.js?version=46926
 // @include      *://instasync.com/r/*
 // @include      *://*.instasync.com/r/*
 // @grant        none
